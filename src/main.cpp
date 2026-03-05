e6520556d734cdb3e3e696b6c48e3909fc7d2c6a
Oleksandr Tsybrivskyi <tsybrivsky@gmail.com>
2026-03-05 11:56:20 +0200

    fead: replace code with windows version

--- src/main.cpp
+++ src/main.cpp
@@ -1,40 +1,67 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <cstring>
+#include <winsock2.h>
+#include <ws2tcpip.h>
 #include <iostream>
 
-int main()
-{
-    int server_fd = socket(AF_INET, SOCK_STREAM, 0);
+#pragma comment(lib, "ws2_32.lib")
 
-    if (server_fd >= 0)
-    {
-        std::cout << "File oppened successfully\n";
+int main() {
+    WSADATA wsaData;
+    if (WSAStartup(MAKEWORD(2,2), &wsaData) != 0) {
+        std::cerr << "WSAStartup failed\n";
+        return 1;
     }
-    else
-    {
-        std::cout << "File Open fail\n";
-        return 0;
+
+    SOCKET server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd == INVALID_SOCKET) {
+        std::cerr << "Socket creation failed\n";
+        WSACleanup();
+        return 1;
     }
+    std::cout << "Socket created successfully\n";
+
     int opt = 1;
-    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt));
+    setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, (const char*)&opt, sizeof(opt));
+
     sockaddr_in address{};
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
     address.sin_port = htons(8080);
-    bind(server_fd, (sockaddr*)&address, sizeof(address));
-    listen(server_fd, 10);
-    while (true)
-    {
-        int client_fd = accept(server_fd, nullptr, nullptr);
-        char buffer[1024];
-        int bytes_read = read(client_fd, buffer, sizeof(buffer));
-        write(client_fd, buffer, bytes_read);
-        close(client_fd);
+
+    if (bind(server_fd, (sockaddr*)&address, sizeof(address)) == SOCKET_ERROR) {
+        std::cerr << "Bind failed\n";
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
     }
 
+    if (listen(server_fd, 10) == SOCKET_ERROR) {
+        std::cerr << "Listen failed\n";
+        closesocket(server_fd);
+        WSACleanup();
+        return 1;
+    }
+
+    std::cout << "Server listening on port 8080...\n";
+
+    while (true) {
+        SOCKET client_fd = accept(server_fd, nullptr, nullptr);
+        if (client_fd == INVALID_SOCKET) {
+            std::cerr << "Accept failed\n";
+            continue;
+        }
+
+        char buffer[1024];
+        int bytes_read = recv(client_fd, buffer, sizeof(buffer), 0);
+        if (bytes_read > 0) {
+            send(client_fd, buffer, bytes_read, 0);
+        }
+
+        closesocket(client_fd);
+    }
+
+    // 6. Очистка
+    closesocket(server_fd);
+    WSACleanup();
 
     return 0;
 }
